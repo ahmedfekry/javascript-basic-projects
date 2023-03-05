32188a8de03dc14bec6b0dc3d08558014d026869
Ahmed Fekry <ahmedfikry78@gmail.com>
2023-03-05 23:24:49 +0200

    finished first javascript project color picker

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let spanColor = document.querySelector('.color');
+
+const btn = document.getElementById('btn').onclick = function(params) {
+    
+    const randomElement = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomElement;
+    spanColor.textContent = randomElement;
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,11 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+document.querySelector('#btn').addEventListener('click',function () { 
+    let color = '#';
+    for(let i=0;i<6;i++){
+        color += hex[Math.floor(Math.random()*hex.length)];
+    }
+
+    document.body.style.backgroundColor = color;
+    document.getElementsByClassName('color')[0].textContent = color;
+});
